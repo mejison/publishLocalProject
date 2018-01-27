29730bdd1de0dbeeead9101dd74e7f58654b5a86
mjs <mjs@div-art.com>
2018-01-27 22:30:45 +0200

    Init commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { remote, ipcRenderer } = require('electron');
+const { handleForm } = remote.require('./index');
+const currentWindow = remote.getCurrentWindow();
+
+const submitFormButton = document.querySelector("#form");
+const responseParagraph = document.getElementById('response');
+
+submitFormButton.addEventListener("submit", function(event){
+    event.preventDefault();
+    let firstname = document.getElementById("firstname").value;
+    alert(firtname)
+});
